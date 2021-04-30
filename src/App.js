f9a19d73a9aab4aa544fa358186ccd9a548fb021
abdullahbilalawan <addbilaleo@gmail.com>
2021-04-30 13:31:42 -0700

    update on component card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      // app instead of App using Block Element Modifier
+
+      const [countries, setCountries] = useState(["Pakistan", "USA", "IRAN"]);
+      const [country, setCountry] = useState(['worldwide'])
+      
+      useEffect(() => {   
+         const getCountriesData = async() => {
+         fetch('https://disease.sh/v3/covid-19/countries/')
+         .then((response) =>   response.json())
+         .then((data) =>{
+            const countries = data.map((country) => (
+                  {name: country.country,
+                   value: country.countryInfo.iso2,  
+                  }
+            ))
+         setCountries(countries);   
+        })
+        
+      };
+      getCountriesData();
+      }, [] )
+
+      const onCountryChange = async(event) =>{
+            const countryCode =  event.target.value;
+            console.log(countryCode)   
+            setCountry(countryCode)    
+      }
+      
+      return (
+            <div className="app">
+                  
+                  <div className="app__header">
+                        <h1> Covid Visualizer</h1>
+                        <FormControl className="app__dropdown">
+                              <Select variant="outlined" onChange = {onCountryChange} value={country}>
+                                    <MenuItem value = "worldwide">
+                                          Worldwide
+                                    </MenuItem>
+                                    {countries.map((country) => (
+                                          <MenuItem value={country.value}>
+                                                {country.name}
+                                          </MenuItem>
+                                    ))}
+                              </Select>
+                        </FormControl>
+                  </div>
+
+                  <div className = "app__stats">
+                        <InfoBox title= "Corona Virus Cases" total={1000} cases={123} />
+                        <InfoBox title= "Recovered"  total={1000} cases={123} />
+                        <InfoBox title= "Deaths"  total={1000} cases={123} />
+
+
+                  </div>
+            </div>
+      );
 }
 
-export default App;
+export default App
---
+++ src/InfoBox.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card,CardContent, Typography } from '@material-ui/core'
+
+export default function InfoBox({title, cases, total }) {
+    return (
+        <div>
+            <Card className = "infoBox">
+                <CardContent>
+                    <Typography color="textSecondary">
+                        {title}
+                    </Typography>
+
+                    <h2 className = "infoBox__cases" >{cases}</h2>
+
+                    <Typography color = "textSecondary">
+                        {total} Total
+                    </Typography>
+
+                </CardContent>
+            </Card>
+            
+        </div>
+    )
+}
